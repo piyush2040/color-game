381c85d6795626b38a5e92b3aea65f3280d0d3f2
piyush2040 <piyushsharma1155@gmail.com>
2020-10-06 16:00:18 +0530

    color game clone

---
+++ colorgame.js
@@ -0,0 +1,56 @@
+var colors = generaterandomcolor(6);
+var squares = document.querySelectorAll(".square");
+var pickedcolor = colors[ran()];
+var colordisplay = document.getElementById("colordisplay");
+var head = document.getElementById("head");
+colordisplay.textContent = pickedcolor;
+var message = document.querySelector("#message");
+for(var i=0;i<squares.length;i++)
+    {
+        squares[i].style.background = colors[i];
+        squares[i].addEventListener("click",function()
+        {
+            var clickedcolor = this.style.background;
+            if(clickedcolor === pickedcolor)
+        {
+                message.textContent = "correct!!";
+                changecolor(pickedcolor);
+                head.style.background = pickedcolor;
+                
+        }
+            else
+                {
+                this.style.background = "#232323";
+                    message.textContent = "incorrect!!";
+                }
+                                    });
+    }
+function changecolor(color)
+{
+    for(var i=0;i<squares.length;i++)
+        {
+            squares[i].style.background = color;
+        }
+}
+function random()
+{
+    var c1 = Math.floor(Math.random()*255);
+    var c2 = Math.floor(Math.random()*255);
+    var c3 = Math.floor(Math.random()*255);
+    
+    return "rgb(" + c1+", " +c2+", " +c3+")";
+}
+function generaterandomcolor(num)
+{
+    arr=[];
+    for(var i=0;i<num;i++)
+        {
+            arr.push(random());
+        }
+    return arr;
+}
+function ran()
+{
+    var c4 = Math.floor(Math.random()*6);
+    return c4;
+}
